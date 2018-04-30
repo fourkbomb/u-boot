da74286a524d55f0187394d19bf6c6fc65ae3f55
Simon Shields <simon@lineageos.org>
2018-04-30 22:59:14 +1000

    i9300: don't pull down the power key
    On reset, GPX2-7 is set to pull-down. This causes the PMIC
    to think that the power key is being pressed, and so causes the PMIC
    to reset the SoC after ~8 seconds (as would happen if you held the power
    key down normally).
    
    To fix this, we do what S-BOOT does: set the input to floating, and rely
    on the external pull-up to make the key work.

--- board/samsung/i9300/i9300.c
+++ board/samsung/i9300/i9300.c
@@ -6,6 +6,8 @@
  */
 
 #include <common.h>
+#include <asm/gpio.h>
+#include <asm/arch/gpio.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -15,8 +17,17 @@ int get_board_rev(void)
 	return 0;
 }
 
+static void board_gpio_init(void)
+{
+	/* GPX2[7] - power key */
+	gpio_request(EXYNOS4X12_GPIO_X27, "nPOWER");
+	gpio_cfg_pin(EXYNOS4X12_GPIO_X27, S5P_GPIO_INPUT);
+	gpio_set_pull(EXYNOS4X12_GPIO_X27, S5P_GPIO_PULL_NONE);
+}
+
 int exynos_init(void)
 {
-	gd->ram_size = 0x4000000;
+	board_gpio_init();
+
 	return 0;
 }
