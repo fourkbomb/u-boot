960b5ee0e1458a8797eae5acffc5422e3e5f59be
Simon Shields <simon@lineageos.org>
2018-05-21 22:23:44 +1000

    power: add battery uclass
    this uclass represents battery devices - they have a "state of charge"
    (capacity %ge), a voltage, and a "state" - not present, ok, or low
    power.
    
    Signed-off-by: Simon Shields <simon@lineageos.org>

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -29,6 +29,7 @@ enum uclass_id {
 	/* U-Boot uclasses start here - in alphabetical order */
 	UCLASS_ADC,		/* Analog-to-digital converter */
 	UCLASS_AHCI,		/* SATA disk controller */
+	UCLASS_BATTERY,		/* Battery */
 	UCLASS_BLK,		/* Block device */
 	UCLASS_CLK,		/* Clock source, e.g. used by peripherals */
 	UCLASS_CPU,		/* CPU, typically part of an SoC */
